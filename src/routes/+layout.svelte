f1501bb69435712a6bbfd1d4f854ef7faf319a4b
punmumu <167979979+punmumu@users.noreply.github.com>
2024-04-26 00:55:51 +0700

    add: loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,15 +10,15 @@ import '../app.pcss';
 </script>
 
 <!-- <div>adgasdg</div> -->
-<!-- {#if loading}
+{#if loading}
 	<div class="flex flex-col h-screen w-full items-center justify-center overflow-hidden">
-		<img  src="https://itcamp20.it.kmitl.ac.th/landing/logoNew.webp" class="animate-spin">
+		<img  src="https://itcamp20.it.kmitl.ac.th/landing/logoNew.webp" class="animate-spin" />
 	</div>
-{:else} -->
-<div class="font-[Prompt] flex flex-col ">
+{:else}
+<div class="font-[Prompt] flex flex-col">
 	<slot></slot>
 </div>
-<!-- {/if} -->
+{/if}
 
 <style>
 	
